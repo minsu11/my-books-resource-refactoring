2f4478a88656aadd3d7312de3512e29a007da459
newjaehun <sjh030798@gmail.com>
2024-02-14 10:48:38 +0900

    feat: book entity 매핑

---
+++ src/main/java/store/mybooks/resource/book_status/entity/BookStatus.java
@@ -0,0 +1,37 @@
+package store.mybooks.resource.book_status.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : store.mybooks.resource.bookstatus.entity
+ * fileName       : BookStatus
+ * author         : newjaehun
+ * date           : 2/13/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/13/24        newjaehun       최초 생성
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "book_status")
+public class BookStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_status_id")
+    private Integer statusId;
+
+    @Column(name = "book_status_name")
+    private String statusName;
+}
