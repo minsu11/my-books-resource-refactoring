0c7b02d4d7e7ba70d6b1860465bb39b3b00cd897
masiljangajji <hjk172262@gmail.com>
2024-02-14 12:56:23 +0900

    feat: Review Entity 매핑

---
+++ src/main/java/store/mybooks/resource/review/entity/Review.java
@@ -0,0 +1,58 @@
+package store.mybooks.resource.review.entity;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import store.mybooks.resource.book.entity.Book;
+import store.mybooks.resource.user.entity.User;
+
+/**
+ * packageName    : store.mybooks.resource.review.entity
+ * fileName       : Review
+ * author         : masiljangajji
+ * date           : 2/13/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/13/24        masiljangajji       최초 생성
+ */
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @Column(name = "review_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="book_id")
+    private Book book;
+
+    @Column(name = "review_rate")
+    private Integer rate;
+
+
+    @Column(name = "review_date")
+    private LocalDate date;
+
+    @Column(name = "review_title")
+    private String title;
+
+    @Column(name = "review_content")
+    private String content;
+
+
+}
