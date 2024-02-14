8af9df2dc384cc0fb92ac1fbb7d45f46c00ef75e
masiljangajji <hjk172262@gmail.com>
2024-02-14 12:57:23 +0900

    feat: UserGrade Entity 매핑

---
+++ src/main/java/store/mybooks/resource/user_grade/entity/UserGrade.java
@@ -0,0 +1,56 @@
+package store.mybooks.resource.user_grade.entity;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : store.mybooks.resource.user.entity
+ * fileName       : UserGrade
+ * author         : masiljangajji
+ * date           : 2/13/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/13/24        masiljangajji       최초 생성
+ */
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name = "user_grade")
+public class UserGrade {
+
+    @Id
+    @Column(name = "user_grade_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_grade_name")
+    private String name;
+
+
+    @Column(name = "user_grade_min")
+    private Integer minCost;
+
+    @Column(name = "user_grade_max")
+    private Integer maxCost;
+
+
+    @Column(name = "user_grade_rate")
+    private Integer rate;
+
+    @Column(name = "user_grade_created_at")
+    private LocalDate createdAt;
+
+
+}
