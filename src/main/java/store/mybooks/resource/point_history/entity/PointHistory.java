de775a721a0ccd78323bdd39d20a48dba87a860a
minsu <zmfjtnl1@naver.com>
2024-02-15 16:25:25 +0900

    feat: PointHistory 생성

---
+++ src/main/java/store/mybooks/resource/point_history/entity/PointHistory.java
@@ -0,0 +1,12 @@
+package store.mybooks.resource.point_history.entity;/**
+ *packageName    : store.mybooks.resource.point_history.entity
+ * fileName       : PointHistory
+ * author         : minsu11
+ * date           : 2/15/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/15/24        minsu11       최초 생성
+ */public class PointHistory {
+}
