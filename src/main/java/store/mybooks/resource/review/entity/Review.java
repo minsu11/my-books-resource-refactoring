160a204e36c2dd84897c4157cff1ef718b9b8541
masiljangajji <hjk172262@gmail.com>
2024-02-15 11:06:10 +0900

    feat: ManyToOne fetch전략 lazy로 변경

--- src/main/java/store/mybooks/resource/review/entity/Review.java
+++ src/main/java/store/mybooks/resource/review/entity/Review.java
@@ -3,6 +3,7 @@ package store.mybooks.resource.review.entity;
 import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -33,11 +34,11 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="user_id")
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="book_id")
     private Book book;
 
