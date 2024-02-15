fb8c60b20b0cdde3dd6a24a1c7669fe7537e94bb
minsu <zmfjtnl1@naver.com>
2024-02-15 21:29:13 +0900

    feat: OrdersStatus repository와 service 파일 생성

---
+++ src/main/java/store/mybooks/resource/orders_status/repository/OrdersStatusRepository.java
@@ -0,0 +1,12 @@
+package store.mybooks.resource.orders_status.repository;/**
+ *packageName    : store.mybooks.resource.orders_status.repository
+ * fileName       : OrderStatusRepository
+ * author         : minsu11
+ * date           : 2/15/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/15/24        minsu11       최초 생성
+ */public interface OrderStatusRepository {
+}
---
+++ src/main/java/store/mybooks/resource/orders_status/service/OrdersStatusService.java
@@ -0,0 +1,12 @@
+package store.mybooks.resource.orders_status.service;/**
+ *packageName    : store.mybooks.resource.orders_status.service
+ * fileName       : OrdersStatusService
+ * author         : minsu11
+ * date           : 2/15/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/15/24        minsu11       최초 생성
+ */public class OrdersStatusService {
+}
