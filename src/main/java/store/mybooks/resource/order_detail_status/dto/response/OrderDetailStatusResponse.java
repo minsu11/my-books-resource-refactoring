04031a066dee67781ae3127985cc8e5c40d10b19
minsu <zmfjtnl1@naver.com>
2024-02-16 00:37:47 +0900

    feat: OrderDetailStatus request, response 구현

---
+++ src/main/java/store/mybooks/resource/order_detail_status/dto/response/OrderDetailStatusResponse.java
@@ -0,0 +1,25 @@
+package store.mybooks.resource.order_detail_status.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : store.mybooks.resource.order_detail_status.dto.response
+ * fileName       : OrderDetailStatusResponse
+ * author         : minsu11
+ * date           : 2/16/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/16/24        minsu11       최초 생성
+ */
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetailStatusResponse {
+    private String id;
+}
