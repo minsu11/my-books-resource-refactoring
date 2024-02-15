93b08bbb8168f809778d1bebc568748a1019b34d
minsu <zmfjtnl1@naver.com>
2024-02-15 21:44:57 +0900

    feat: OrdersStatus dto 생성 및 구현

---
+++ src/main/java/store/mybooks/resource/orders_status/dto/response/OrdersStatusResponse.java
@@ -0,0 +1,23 @@
+package store.mybooks.resource.orders_status.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : store.mybooks.resource.orders_status.dto.response
+ * fileName       : OrdersStatusResponse
+ * author         : minsu11
+ * date           : 2/15/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/15/24        minsu11       최초 생성
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrdersStatusResponse {
+    private String id;
+}
