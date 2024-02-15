2c4c3d30356705f99340e3fefcf0ac041ad18481
minsu <zmfjtnl1@naver.com>
2024-02-15 16:26:28 +0900

    feat: PointHistory 엔티티 매핑

--- src/main/java/store/mybooks/resource/point_history/entity/PointHistory.java
+++ src/main/java/store/mybooks/resource/point_history/entity/PointHistory.java
@@ -1,5 +1,16 @@
-package store.mybooks.resource.point_history.entity;/**
- *packageName    : store.mybooks.resource.point_history.entity
+package store.mybooks.resource.point_history.entity;
+
+import java.time.LocalDate;
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import store.mybooks.resource.book_order.entity.BookOrder;
+import store.mybooks.resource.point_rule.entity.PointRule;
+import store.mybooks.resource.user.entity.User;
+
+/**
+ * packageName    : store.mybooks.resource.point_history.entity
  * fileName       : PointHistory
  * author         : minsu11
  * date           : 2/15/24
@@ -8,5 +19,33 @@ package store.mybooks.resource.point_history.entity;/**
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2/15/24        minsu11       최초 생성
- */public class PointHistory {
+ */
+@Getter
+@Entity
+@Table(name = "point_history")
+@NoArgsConstructor
+@AllArgsConstructor
+public class PointHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "point_history_id")
+    private Long id;
+
+    @Column(name = "point_status_cost")
+    private Integer pointStatusCost;
+
+    @Column(name = "point_history_created_date")
+    private LocalDate createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private BookOrder bookOrder;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "point_rule_id")
+    private PointRule pointRule;
 }
