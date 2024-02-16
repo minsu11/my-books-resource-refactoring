c787c684c69a4f994ce31a9605ab694ad90a8438
masiljangajji <hjk172262@gmail.com>
2024-02-16 14:11:20 +0900

    faet: UserStatus Repo,Service 추가

---
+++ src/main/java/store/mybooks/resource/user_status/service/UserStatusService.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.user_status.service;
+
+/**
+ * packageName    : store.mybooks.resource.user_status.service
+ * fileName       : UserStatusService
+ * author         : masiljangajji
+ * date           : 2/13/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/13/24        masiljangajji       최초 생성
+ */
+public class UserStatusService {
+}
