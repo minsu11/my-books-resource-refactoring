b64c87804ecbe25212374e82f396477a03275571
masiljangajji <hjk172262@gmail.com>
2024-02-16 10:26:22 +0900

    feat: User C,U,D Response 구현

---
+++ src/main/java/store/mybooks/resource/user/dto/response/UserDeleteResponse.java
@@ -0,0 +1,23 @@
+package store.mybooks.resource.user.dto.response;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * packageName    : store.mybooks.resource.user.dto.common
+ * fileName       : DeleteResponse
+ * author         : masiljangajji
+ * date           : 2/13/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/13/24        masiljangajji       최초 생성
+ */
+
+
+@AllArgsConstructor
+public class UserDeleteResponse {
+
+    private String message;
+
+}
