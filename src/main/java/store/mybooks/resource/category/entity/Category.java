b2372cac4d02a05497b8f0574ad652f33e90a701
damho <ydh4745@gamil.com>
2024-02-16 13:14:46 +0900

    refactor: parentCategoryId -> parentCategory 로 변수명 변경

--- src/main/java/store/mybooks/resource/category/entity/Category.java
+++ src/main/java/store/mybooks/resource/category/entity/Category.java
@@ -2,7 +2,16 @@ package store.mybooks.resource.category.entity;
 
 import java.time.LocalDate;
 import java.util.List;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 /**
  * packageName    : store.mybooks.resource.category.entity
@@ -25,9 +34,9 @@ public class Category {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_category_id")
-    private Category parentCategoryId;
+    private Category parentCategory;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "parentCategoryId")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "parentCategory")
     private List<Category> childCategoryList;
 
     @Column(name = "category_name")
