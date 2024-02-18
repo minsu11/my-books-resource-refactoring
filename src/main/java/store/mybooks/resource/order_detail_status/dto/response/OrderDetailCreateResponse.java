b128eb52dca636250222f146add243ab2bbdf4e6
minsu <zmfjtnl1@naver.com>
2024-02-18 23:03:38 +0900

    feat: OrderDetail Create Response/Create Request 구현

---
+++ src/main/java/store/mybooks/resource/order_detail_status/dto/response/OrderDetailCreateResponse.java
@@ -0,0 +1,26 @@
+package store.mybooks.resource.order_detail_status.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * packageName    : store.mybooks.resource.order_detail_status.dto.response
+ * fileName       : OrderDetailCreateResponse
+ * author         : minsu11
+ * date           : 2/18/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/18/24        minsu11       최초 생성
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetailCreateResponse {
+    private String id;
+
+}
