e994f42f547e44f0e828c0f17043919def1c3158
minsu <zmfjtnl1@naver.com>
2024-02-18 17:20:47 +0900

    test: gateway와 연동 테스트

--- src/main/java/store/mybooks/resource/TestController.java
+++ src/main/java/store/mybooks/resource/TestController.java
@@ -20,6 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public String test() {
-        return "test222";
+        return "test2222";
     }
 }
