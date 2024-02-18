d5813b81fa1f72bc840d6d145307bf7bdfaa25d1
hyeonjaez <jhjh0022@naver.com>
2024-02-19 02:04:29 +0900

    feat: registerCart, getCart 기능 추가

--- src/main/java/store/mybooks/resource/cart/service/CartService.java
+++ src/main/java/store/mybooks/resource/cart/service/CartService.java
@@ -3,9 +3,15 @@ package store.mybooks.resource.cart.service;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import store.mybooks.resource.cart.dto.CartDto;
+import store.mybooks.resource.cart.dto.CartMapper;
 import store.mybooks.resource.cart.dto.CartRegisterRequest;
+import store.mybooks.resource.cart.dto.CartResponse;
 import store.mybooks.resource.cart.entity.Cart;
+import store.mybooks.resource.cart.exception.CartAlreadyExistException;
+import store.mybooks.resource.cart.exception.CartNotFoundException;
 import store.mybooks.resource.cart.repository.CartRepository;
+import store.mybooks.resource.user.entity.User;
 import store.mybooks.resource.user.entity.UserRepository;
 
 /**
@@ -24,24 +30,31 @@ import store.mybooks.resource.user.entity.UserRepository;
 public class CartService {
     private final CartRepository cartRepository;
     private final UserRepository userRepository;
+    private final CartMapper cartMapper;
 
 
-    public void createCart(CartRegisterRequest cartRegisterRequest) {
-        Optional<Cart> optionalCart = cartRepository.findCartByUserId(cartRegisterRequest.getUserId());
-        if (optionalCart.isPresent()) {
+    public CartResponse registerCart(CartRegisterRequest cartRegisterRequest) {
+        Optional<User> optionalUser = userRepository.findById(cartRegisterRequest.getUserId());
 
+        if (optionalUser.isEmpty()) {
+            throw new RuntimeException("");
+        } else if (cartRepository.existsCartByUserId(cartRegisterRequest.getUserId())) {
+            throw new CartAlreadyExistException("");
         } else {
-
-            return;
+            User user = optionalUser.get();
+            Cart cart = new Cart(user);
+            Cart saveCart = cartRepository.save(cart);
+            return cartMapper.mapToResponse(saveCart);
         }
-
-    }
-
-    public Cart getCart(Long userId) {
-        Optional<Cart> optionalCart = cartRepository.findCartByUserId(userId);
-
-        return optionalCart.orElseThrow(null);
     }
 
 
+    public CartDto getCart(Long userId) {
+        Optional<CartDto> optionalCart = cartRepository.findCartByUserId(userId);
+        if (optionalCart.isPresent()) {
+            return optionalCart.get();
+        } else {
+            throw new CartNotFoundException("");
+        }
+    }
 }
