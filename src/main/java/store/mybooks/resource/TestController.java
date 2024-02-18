ec4f6c279a61000f14e97d30a7566df6ebec4466
newjaehun <sjh030798@gmail.com>
2024-02-18 15:06:17 +0900

    feat: Test용 TestController추가

---
+++ src/main/java/store/mybooks/resource/TestController.java
@@ -0,0 +1,25 @@
+package store.mybooks.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : store.mybooks.resource
+ * fileName       : TestController
+ * author         : newjaehun
+ * date           : 2/18/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/18/24        newjaehun       최초 생성
+ */
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "test222";
+    }
+}
