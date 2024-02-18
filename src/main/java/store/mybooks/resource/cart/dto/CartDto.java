65d6fcd9e8d7b607a51ebc26d3b010fb5fc18e1f
hyeonjaez <jhjh0022@naver.com>
2024-02-19 02:02:36 +0900

    feat: CartDto 추가

---
+++ src/main/java/store/mybooks/resource/cart/dto/CartDto.java
@@ -0,0 +1,18 @@
+package store.mybooks.resource.cart.dto;
+
+/**
+ * packageName    : store.mybooks.resource.cart.dto
+ * fileName       : CartDto
+ * author         : Fiat_lux
+ * date           : 2/18/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/18/24        Fiat_lux       최초 생성
+ */
+public interface CartDto {
+    Long getId();
+
+    Long getUserId();
+}
