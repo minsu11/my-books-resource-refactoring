a5d9ca46eb3e6e8b2c5860a954b9346748aba176
masiljangajji <hjk172262@gmail.com>
2024-02-19 02:26:41 +0900

    feat: UserStatus CreateRequest 및 Response 추가

---
+++ src/main/java/store/mybooks/resource/user_status/dto/response/UserStatusCreateResponse.java
@@ -0,0 +1,23 @@
+package store.mybooks.resource.user_status.dto.response;
+
+import lombok.Data;
+
+/**
+ * packageName    : store.mybooks.resource.user_status.dto.response
+ * fileName       : UserStatusCreateResponse
+ * author         : masiljangajji
+ * date           : 2/18/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/18/24        masiljangajji       최초 생성
+ */
+
+
+@Data
+public class UserStatusCreateResponse {
+
+    String id;
+
+}
