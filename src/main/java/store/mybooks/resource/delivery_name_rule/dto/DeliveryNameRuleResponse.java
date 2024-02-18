58b8be646a34ec667255cd9e807a0611b261c228
hyeonjaez <jhjh0022@naver.com>
2024-02-19 01:47:00 +0900

    fix: @Builder, @AllArgsConstructor 삭제

--- src/main/java/store/mybooks/resource/delivery_name_rule/dto/DeliveryNameRuleResponse.java
+++ src/main/java/store/mybooks/resource/delivery_name_rule/dto/DeliveryNameRuleResponse.java
@@ -1,10 +1,9 @@
 package store.mybooks.resource.delivery_name_rule.dto;
 
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * packageName    : store.mybooks.resource.delivery_name_rule.dto
@@ -17,10 +16,10 @@ import lombok.NoArgsConstructor;
  * -----------------------------------------------------------
  * 2/16/24        Fiat_lux       최초 생성
  */
-@Builder
+
 @Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class DeliveryNameRuleResponse {
     private Integer id;
     private String name;
