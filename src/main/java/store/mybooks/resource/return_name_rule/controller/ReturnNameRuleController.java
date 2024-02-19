945eace007394f0a9ec390adcf6cb93f971a2e6e
minsu <zmfjtnl1@naver.com>
2024-02-20 01:46:57 +0900

    feat: ReturnNameRuleController 생성

---
+++ src/main/java/store/mybooks/resource/return_name_rule/controller/ReturnNameRuleController.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.return_name_rule.controller;
+
+/**
+ * packageName    : store.mybooks.resource.return_name_rule.controller<br>
+ * fileName       : ReturnNameRuleController<br>
+ * author         : minsu11<br>
+ * date           : 2/20/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/20/24        minsu11       최초 생성<br>
+ */
+public class ReturnNameRuleController {
+}
