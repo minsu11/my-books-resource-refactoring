66ddffc3c3c6ce5928a9f7e7b16bc51d1134c780
minsu <zmfjtnl1@naver.com>
2024-02-20 01:50:29 +0900

    feat: ReturnNameRule Controller getReturnNameRule 메서드 구현

--- src/main/java/store/mybooks/resource/return_name_rule/controller/ReturnNameRuleController.java
+++ src/main/java/store/mybooks/resource/return_name_rule/controller/ReturnNameRuleController.java
@@ -1,5 +1,15 @@
 package store.mybooks.resource.return_name_rule.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import store.mybooks.resource.return_name_rule.dto.response.ReturnNameRuleResponse;
+import store.mybooks.resource.return_name_rule.service.ReturnNameRuleService;
+
 /**
  * packageName    : store.mybooks.resource.return_name_rule.controller<br>
  * fileName       : ReturnNameRuleController<br>
@@ -11,5 +21,15 @@ package store.mybooks.resource.return_name_rule.controller;
  * -----------------------------------------------------------<br>
  * 2/20/24        minsu11       최초 생성<br>
  */
+@RestController
+@RequestMapping("/api/return-name-rule")
+@RequiredArgsConstructor
 public class ReturnNameRuleController {
+    private final ReturnNameRuleService returnNameRuleService;
+
+    @GetMapping("/{name}")
+    public ResponseEntity<ReturnNameRuleResponse> getReturnNameRule(@PathVariable(name = "name") String name) {
+        ReturnNameRuleResponse response = returnNameRuleService.getReturnNameRule(name);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
