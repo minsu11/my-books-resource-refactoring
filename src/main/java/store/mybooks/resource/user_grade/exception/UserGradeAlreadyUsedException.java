020029a0d0528ae8eeafe91351cc26239504234f
masiljangajji <hjk172262@gmail.com>
2024-02-19 14:04:04 +0900

    refactor: UserGradeException 변경

--- src/main/java/store/mybooks/resource/user_grade/exception/UserGradeAlreadyUsedException.java
+++ src/main/java/store/mybooks/resource/user_grade/exception/UserGradeAlreadyUsedException.java
@@ -2,17 +2,18 @@ package store.mybooks.resource.user_grade.exception;
 
 /**
  * packageName    : store.mybooks.resource.user_grade.exception
- * fileName       : UserGradeNotExistException
+ * fileName       : UserGradeNotAvailableException
  * author         : masiljangajji
- * date           : 2/16/24
+ * date           : 2/19/24
  * description    :
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
- * 2/16/24        masiljangajji       최초 생성
+ * 2/19/24        masiljangajji       최초 생성
  */
-public class UserGradeNotExistException extends RuntimeException {
-    public UserGradeNotExistException(Integer userGradeName) {
-        super(String.format("[%d]는 존재하지 않는 User Grade 입니다.", userGradeName));
+public class UserGradeAlreadyUsedException extends RuntimeException {
+
+    public UserGradeAlreadyUsedException(String userGradeName) {
+        super(String.format("[%s]는 이미 사용중입니다", userGradeName));
     }
 }
