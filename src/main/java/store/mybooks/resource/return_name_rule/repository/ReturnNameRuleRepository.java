9d48f4a3d1c189d40623810dd943d1b6a98b58e5
minsu <zmfjtnl1@naver.com>
2024-02-20 01:25:27 +0900

    feat: ReturnNameRuleRepository 생성

---
+++ src/main/java/store/mybooks/resource/return_name_rule/repository/ReturnNameRuleRepository.java
@@ -0,0 +1,16 @@
+package store.mybooks.resource.return_name_rule.repository;
+
+/**
+ * packageName    : store.mybooks.resource.return_name_rule.repository
+ * fileName       : ReturnNameRuleRepository
+ * author         : minsu11
+ * date           : 2/20/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/20/24        minsu11       최초 생성
+ */
+
+public interface ReturnNameRuleRepository {
+}
