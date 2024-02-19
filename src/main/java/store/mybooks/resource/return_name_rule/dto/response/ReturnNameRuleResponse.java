21debe195c9528db959966bf3d612a7fe02ba1c1
minsu <zmfjtnl1@naver.com>
2024-02-20 01:24:45 +0900

    feat: ReturnNameRule dto 및 mapper interface 구현

--- src/main/java/store/mybooks/resource/return_name_rule/dto/response/ReturnNameRuleResponse.java
+++ src/main/java/store/mybooks/resource/return_name_rule/dto/response/ReturnNameRuleResponse.java
@@ -1,15 +1,27 @@
 package store.mybooks.resource.return_name_rule.dto.response;
 
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
- * packageName    : store.mybooks.resource.return_name_rule.dto.response
- * fileName       : ReturnNameRuleResponse
- * author         : minsu11
- * date           : 2/20/24
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2/20/24        minsu11       최초 생성
+ * packageName    : store.mybooks.resource.return_name_rule.dto.response<br>
+ * fileName       : ReturnNameRuleResponse<br>
+ * author         : minsu11<br>
+ * date           : 2/20/24<br>
+ * description    : 요청된 반품 규정 명 규칙 응답 데이터 담은 dto<br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/20/24        minsu11       최초 생성<br>
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReturnNameRuleResponse {
+    private String name;
+    private LocalDate localDate;
 }
