3702c3eab90f95774aeb02add10278eea8171657
minsu <zmfjtnl1@naver.com>
2024-02-20 01:55:35 +0900

    docs: ReturnNameRuleController getReturnNameRule javadoc 작성

--- src/main/java/store/mybooks/resource/return_name_rule/controller/ReturnNameRuleController.java
+++ src/main/java/store/mybooks/resource/return_name_rule/controller/ReturnNameRuleController.java
@@ -27,6 +27,14 @@ import store.mybooks.resource.return_name_rule.service.ReturnNameRuleService;
 public class ReturnNameRuleController {
     private final ReturnNameRuleService returnNameRuleService;
 
+    /**
+     * methodName : getReturnNameRule<br>
+     * author : minsu11<br>
+     * description : get 요청 시 name 값에 맞는 데이터를 응답
+     *
+     * @param name url 경로로 들어온 요청 데이터
+     * @return response entity
+     */
     @GetMapping("/{name}")
     public ResponseEntity<ReturnNameRuleResponse> getReturnNameRule(@PathVariable(name = "name") String name) {
         ReturnNameRuleResponse response = returnNameRuleService.getReturnNameRule(name);
