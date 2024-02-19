00d0ff751dae8795b7fc343f18618c7c9e27dd0e
minsu <zmfjtnl1@naver.com>
2024-02-20 01:10:48 +0900

    feat: 반품 규정 명 규칙 dto 생성

---
+++ src/main/java/store/mybooks/resource/return_name_rule/dto/request/ReturnNameRuleRequest.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.return_name_rule.dto.request;
+
+/**
+ * packageName    : store.mybooks.resource.return_name_rule.dto.request
+ * fileName       : ReturnNameRuleRequest
+ * author         : minsu11
+ * date           : 2/20/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/20/24        minsu11       최초 생성
+ */
+public class ReturnNameRuleRequest {
+}
---
+++ src/main/java/store/mybooks/resource/return_name_rule/dto/response/ReturnNameRuleResponse.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.return_name_rule.dto.response;
+
+/**
+ * packageName    : store.mybooks.resource.return_name_rule.dto.response
+ * fileName       : ReturnNameRuleResponse
+ * author         : minsu11
+ * date           : 2/20/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/20/24        minsu11       최초 생성
+ */
+public class ReturnNameRuleResponse {
+}
