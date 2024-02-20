4e6e3eb37aa1162b7a59d0adaccfc2d38b241602
hyeonjaez <jhjh0022@naver.com>
2024-02-20 10:43:28 +0900

    doc: java doc 추가

--- src/main/java/store/mybooks/resource/cart/repository/CartRepository.java
+++ src/main/java/store/mybooks/resource/cart/repository/CartRepository.java
@@ -17,7 +17,20 @@ import store.mybooks.resource.cart.entity.Cart;
  * 2/15/24        Fiat_lux       최초 생성
  */
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    /**
+     * Exists cart by user id boolean.
+     *
+     * @param userId the user id
+     * @return the boolean
+     */
     Boolean existsCartByUserId(Long userId);
 
+    /**
+     * Find cart by user id optional.
+     *
+     * @param userId the user id
+     * @return the optional
+     */
     Optional<CartDto> findCartByUserId(Long userId);
 }
