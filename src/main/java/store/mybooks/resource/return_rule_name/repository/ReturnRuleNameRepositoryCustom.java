aed1ff43f1ac95dcbcb9e0708a9b89e51f828196
minsu <zmfjtnl1@naver.com>
2024-02-21 01:06:29 +0900

    feat: Quert dsl 사용 하기 위한 ReturnRuleName Repository Custom 생성

---
+++ src/main/java/store/mybooks/resource/return_rule_name/repository/ReturnRuleNameRepositoryCustom.java
@@ -0,0 +1,12 @@
+package store.mybooks.resource.return_rule_name.repository;/**
+ *packageName    : store.mybooks.resource.return_rule_name.repository<br>
+ * fileName       : ReturnRuleNameRepositoryCustom<br>
+ * author         : minsu11<br>
+ * date           : 2/21/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/21/24        minsu11       최초 생성<br>
+ */public interface ReturnRuleNameRepositoryCustom {
+}
