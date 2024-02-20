f1e2dfe65ed0bec4160b23cda25c98ba37ee305a
hyeonjaez <jhjh0022@naver.com>
2024-02-20 19:15:21 +0900

    test: unit test, displayname, 이름

--- src/test/java/store/mybooks/resource/delivery_name_rule/repository/DeliveryRuleNameRepositoryTest.java
+++ src/test/java/store/mybooks/resource/delivery_name_rule/repository/DeliveryRuleNameRepositoryTest.java
@@ -3,16 +3,14 @@ package store.mybooks.resource.delivery_name_rule.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.time.LocalDate;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.transaction.annotation.Transactional;
-import store.mybooks.resource.delivery_name_rule.dto.DeliveryNameRuleDto;
-import store.mybooks.resource.delivery_name_rule.entity.DeliveryNameRule;
+import store.mybooks.resource.delivery_name_rule.dto.DeliveryRuleNameDto;
+import store.mybooks.resource.delivery_rule_name.entity.DeliveryRuleName;
 
 /**
  * packageName    : store.mybooks.resource.delivery_name_rule.repository
@@ -26,31 +24,29 @@ import store.mybooks.resource.delivery_name_rule.entity.DeliveryNameRule;
  * 2/17/24        Fiat_lux       최초 생성
  */
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class DeliveryNameRuleRepositoryTest {
+class DeliveryRuleNameRepositoryTest {
 
     @Autowired
-    private DeliveryNameRuleRepository deliveryNameRuleRepository;
-    private static DeliveryNameRule deliveryNameRule;
-    private static DeliveryNameRule saveDeliveryNameRule;
+    private DeliveryRuleNameRepository deliveryRuleNameRepository;
+    private static DeliveryRuleName deliveryNameRule;
+    private static DeliveryRuleName saveDeliveryNameRule;
 
     @BeforeEach
     void setup() {
-        deliveryNameRule = new DeliveryNameRule(1, "test", LocalDate.now());
-        saveDeliveryNameRule = deliveryNameRuleRepository.save(deliveryNameRule);
+        deliveryNameRule = new DeliveryRuleName("test");
+        saveDeliveryNameRule = deliveryRuleNameRepository.save(deliveryNameRule);
     }
 
     @Test
-    @Transactional
-    void findDeliveryNameRuleByIdTest() {
-        Optional<DeliveryNameRuleDto> resultDto = deliveryNameRuleRepository.findDeliveryNameRuleById(
+    @DisplayName("DeliveryRuleName read 테스트")
+    void givenDeliveryRuleNameId_whenFindDeliveryRuleNameById_thenReturnDeliveryRuleNameDto() {
+        Optional<DeliveryRuleNameDto> resultDto = deliveryRuleNameRepository.findDeliveryRuleNameById(
                 saveDeliveryNameRule.getId());
 
         assertTrue(resultDto.isPresent());
-        DeliveryNameRuleDto resultDeliveryNameRuleDto = resultDto.get();
+        DeliveryRuleNameDto resultDeliveryNameRuleDto = resultDto.get();
 
         assertEquals(deliveryNameRule.getId(), resultDeliveryNameRuleDto.getId());
-        assertEquals(deliveryNameRule.getName(), resultDeliveryNameRuleDto.getName());
         assertEquals(deliveryNameRule.getCreatedDate(), resultDeliveryNameRuleDto.getCreatedDate());
     }
 }
