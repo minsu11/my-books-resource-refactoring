2712d883fb1c22d5eab586a53b3e67d247d6b56b
minsu <zmfjtnl1@naver.com>
2024-02-20 22:33:05 +0900

    feat: OrderDetailStatus Service unit test 생성

---
+++ src/test/java/store/mybooks/resource/order_detail_status/service/OrderDetailStatusServiceUnitTest.java
@@ -0,0 +1,30 @@
+package store.mybooks.resource.order_detail_status.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * packageName    : store.mybooks.resource.order_detail_status.service<br>
+ * fileName       : OrderDetailStatusServiceTest<br>
+ * author         : minsu11<br>
+ * date           : 2/20/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/20/24        minsu11       최초 생성<br>
+ */
+class OrderDetailStatusServiceUnitTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getOrderDetailStatus() {
+    }
+
+    @Test
+    void getOrderDetailStatusList() {
+    }
+}
