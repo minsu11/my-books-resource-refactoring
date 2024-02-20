08991a924948c59b1378a47579626cc651b2e23a
newjaehun <sjh030798@gmail.com>
2024-02-20 13:14:52 +0900

    feat: AuthorDTO

---
+++ src/main/java/store/mybooks/resource/author/dto/response/AuthorGetResponse.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.author.dto.response;
+
+/**
+ * packageName    : store.mybooks.resource.author.dto.response
+ * fileName       : AuthorGetResponse
+ * author         : newjaehun
+ * date           : 2/20/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/20/24        newjaehun       최초 생성
+ */
+public interface AuthorGetResponse {
+}
