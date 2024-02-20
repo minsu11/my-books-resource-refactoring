74a18d72241e33b3a238dce941c1eddc7c553a50
minsu <zmfjtnl1@naver.com>
2024-02-20 21:53:57 +0900

    feat: OrderDetailStatusController Unit test 생성

---
+++ src/test/java/store/mybooks/resource/order_detail_status/controller/OrderDetailStatusControllerUnitTest.java
@@ -0,0 +1,25 @@
+package store.mybooks.resource.order_detail_status.controller;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * packageName    : store.mybooks.resource.order_detail_status.controller<br>
+ * fileName       : OrderDetailStatusControllerTest<br>
+ * author         : minsu11<br>
+ * date           : 2/20/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/20/24        minsu11       최초 생성<br>
+ */
+class OrderDetailStatusControllerUnitTest {
+
+    @Test
+    void getOrderDetailStatusByOrderDetailStatusId() {
+    }
+
+    @Test
+    void getOrderDetailStatusList() {
+    }
+}
