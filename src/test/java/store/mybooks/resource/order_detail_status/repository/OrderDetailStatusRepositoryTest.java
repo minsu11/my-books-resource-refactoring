0463e03351570bec7e93ca5cac78982d53b5b9ad
minsu <zmfjtnl1@naver.com>
2024-02-21 00:23:44 +0900

    feat: OrderDetailStatus Repository Test 생성

---
+++ src/test/java/store/mybooks/resource/order_detail_status/repository/OrderDetailStatusRepositoryTest.java
@@ -0,0 +1,21 @@
+package store.mybooks.resource.order_detail_status.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * packageName    : store.mybooks.resource.order_detail_status.repository<br>
+ * fileName       : OrderDetailStatusRepositoryTest<br>
+ * author         : minsu11<br>
+ * date           : 2/21/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/21/24        minsu11       최초 생성<br>
+ */
+class OrderDetailStatusRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
