fa48a17108cfedc329148061c3908f36a0e3cc32
masiljangajji <hjk172262@gmail.com>
2024-02-21 16:57:36 +0900

    feat: UserAddress Repo,Controller 비어있는 Test 추가

---
+++ src/test/java/store/mybooks/resource/user_address/controller/UserAddressRestControllerTest.java
@@ -0,0 +1,18 @@
+package store.mybooks.resource.user_address.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * packageName    : store.mybooks.resource.user_address.controller
+ * fileName       : UserAddressRestControllerTest
+ * author         : masiljangajji
+ * date           : 2/21/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/21/24        masiljangajji       최초 생성
+ */
+class UserAddressRestControllerTest {
+
+}
---
+++ src/test/java/store/mybooks/resource/user_address/repository/UserAddressRepositoryTest.java
@@ -0,0 +1,18 @@
+package store.mybooks.resource.user_address.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * packageName    : store.mybooks.resource.user_address.repository
+ * fileName       : UserAddressRepositoryTest
+ * author         : masiljangajji
+ * date           : 2/21/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/21/24        masiljangajji       최초 생성
+ */
+class UserAddressRepositoryTest {
+
+}
