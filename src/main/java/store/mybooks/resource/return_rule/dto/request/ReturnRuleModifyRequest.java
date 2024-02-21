bbd8eb23efe1fbc57536e95469d6d4ce2b2159a0
minsu <zmfjtnl1@naver.com>
2024-02-22 01:16:21 +0900

    feat: ReturnRule Modify dto 생성

---
+++ src/main/java/store/mybooks/resource/return_rule/dto/request/ReturnRuleModifyRequest.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.return_rule.dto.request;
+
+/**
+ * packageName    : store.mybooks.resource.return_rule.dto.request<br>
+ * fileName       : ReturnRuleModifyRequest<br>
+ * author         : minsu11<br>
+ * date           : 2/22/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/22/24        minsu11       최초 생성<br>
+ */
+public class ReturnRuleModifyRequest {
+}
---
+++ src/main/java/store/mybooks/resource/return_rule/dto/response/ReturnRuleModifyResponse.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.return_rule.dto.response;
+
+/**
+ * packageName    : store.mybooks.resource.return_rule.dto.response<br>
+ * fileName       : ReturnRuleModifyResponse<br>
+ * author         : minsu11<br>
+ * date           : 2/22/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/22/24        minsu11       최초 생성<br>
+ */
+public class ReturnRuleModifyResponse {
+}
