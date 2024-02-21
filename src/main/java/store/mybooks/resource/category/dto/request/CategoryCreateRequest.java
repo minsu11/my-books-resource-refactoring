0f5f948007b971870e55d527b90f384cac0ca926
damho <ydh4745@gamil.com>
2024-02-21 19:25:05 +0900

    feat: CategoryCreateRequest.java Validation 추가

--- src/main/java/store/mybooks/resource/category/dto/request/CategoryCreateRequest.java
+++ src/main/java/store/mybooks/resource/category/dto/request/CategoryCreateRequest.java
@@ -1,7 +1,9 @@
 package store.mybooks.resource.category.dto.request;
 
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * packageName    : store.mybooks.resource.category.dto.request
@@ -18,5 +20,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CategoryCreateRequest {
     private Integer parentCategoryId;
+
+    @NotBlank
+    @Length(min = 1, max = 20)
     private String name;
 }
