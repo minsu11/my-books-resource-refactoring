c3e22ac8a5cf0a21336327a02849bb5893fcadb7
minsu <zmfjtnl1@naver.com>
2024-02-21 16:26:29 +0900

    feat: return rule repository service 생성

---
+++ src/main/java/store/mybooks/resource/return_rule/service/ReturnRuleService.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.return_rule.service;
+
+/**
+ * packageName    : store.mybooks.resource.return_rule.service<br>
+ * fileName       : ReturnRuleService<br>
+ * author         : minsu11<br>
+ * date           : 2/21/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/21/24        minsu11       최초 생성<br>
+ */
+public class ReturnRuleService {
+}
