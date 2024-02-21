1c67f9eaa761cd4e8a45530ec63abee38ddd2e30
masiljangajji <hjk172262@gmail.com>
2024-02-21 16:57:12 +0900

    feat: UserAddressService Test 추가

---
+++ src/test/java/store/mybooks/resource/user_address/service/UserAddressServiceTest.java
@@ -0,0 +1,18 @@
+package store.mybooks.resource.user_address.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * packageName    : store.mybooks.resource.user_address.service
+ * fileName       : UserAddressServiceTest
+ * author         : masiljangajji
+ * date           : 2/21/24
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2/21/24        masiljangajji       최초 생성
+ */
+class UserAddressServiceTest {
+
+}
