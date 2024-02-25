08abe80e1971e376348cfb5130340adba06dce35
hyeonjaez <jhjh0022@naver.com>
2024-02-26 03:14:31 +0900

    refactor: 유효성 검사

---
+++ src/main/java/store/mybooks/resource/delivery_rule/dto/request/DeliveryRuleModifyRequest.java
@@ -0,0 +1,31 @@
+package store.mybooks.resource.delivery_rule.dto.request;
+
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * packageName    : store.mybooks.resource.delivery_rule.dto<br>
+ * fileName       : DeliveryRuleModifyRequest<br>
+ * author         : Fiat_lux<br>
+ * date           : 2/16/24<br>
+ * description    : 수정할 정보가 들어 있는 배송 규칙 DTO<br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/16/24        Fiat_lux       최초 생성<br>
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryRuleModifyRequest {
+    @NotBlank
+    private String deliveryNameRuleId;
+    @NotBlank
+    private String deliveryRuleCompanyName;
+    private Integer deliveryCost;
+    private Integer deliveryRuleCost;
+}
---
+++ src/main/java/store/mybooks/resource/delivery_rule/dto/request/DeliveryRuleRegisterRequest.java
@@ -0,0 +1,29 @@
+package store.mybooks.resource.delivery_rule.dto.request;
+
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : store.mybooks.resource.delivery_rule.dto<br>
+ * fileName       : DeliveryRuleRegisterRequest<br>
+ * author         : Fiat_lux<br>
+ * date           : 2/16/24<br>
+ * description    : 등록할 정보가 들어 있는 배송 규칙 DTO<br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/16/24        Fiat_lux       최초 생성<br>
+ */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryRuleRegisterRequest {
+    @NotBlank
+    private String deliveryNameRuleId;
+    @NotBlank
+    private String deliveryCompanyName;
+    private Integer deliveryCost;
+    private Integer deliveryRuleCost;
+}
