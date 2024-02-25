145211ddf33809f9094c37349c8ff0577295a75d
hyeonjaez <jhjh0022@naver.com>
2024-02-26 02:29:17 +0900

    refactor: request validation, 자바독 수정

--- src/main/java/store/mybooks/resource/delivery_rule_name/controller/DeliveryRuleNameController.java
+++ src/main/java/store/mybooks/resource/delivery_rule_name/controller/DeliveryRuleNameController.java
@@ -1,8 +1,10 @@
 package store.mybooks.resource.delivery_rule_name.controller;
 
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,21 +12,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import store.mybooks.resource.delivery_rule_name.dto.DeliveryRuleNameDto;
-import store.mybooks.resource.delivery_rule_name.dto.DeliveryRuleNameRegisterRequest;
-import store.mybooks.resource.delivery_rule_name.dto.DeliveryRuleNameResponse;
+import store.mybooks.resource.delivery_rule_name.dto.request.DeliveryRuleNameRegisterRequest;
+import store.mybooks.resource.delivery_rule_name.dto.response.DeliveryRuleNameDto;
+import store.mybooks.resource.delivery_rule_name.dto.response.DeliveryRuleNameResponse;
+import store.mybooks.resource.delivery_rule_name.exception.DeliveryRuleNameValidationFailedException;
 import store.mybooks.resource.delivery_rule_name.service.DeliveryRuleNameService;
 
 /**
- * packageName    : store.mybooks.resource.delivery_name_rule.controller
- * fileName       : DeliveryNameRuleController
- * author         : Fiat_lux
- * date           : 2/16/24
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2/16/24        Fiat_lux       최초 생성
+ * packageName    : store.mybooks.resource.delivery_name_rule.controller<br>
+ * fileName       : DeliveryNameRuleController<br>
+ * author         : Fiat_lux<br>
+ * date           : 2/16/24<br>
+ * description    :<br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/16/24        Fiat_lux       최초 생성<br>
  */
 @RestController
 @RequiredArgsConstructor
@@ -34,10 +37,12 @@ public class DeliveryRuleNameController {
     private final DeliveryRuleNameService deliveryNameRuleService;
 
     /**
-     * Gets delivery name rule.
+     * methodName : getDeliveryNameRule<br>
+     * author : Fiat_lux<br>
+     * description : get 요청으로 파라미터 값 id에 대한 조회 데이터 응답<br>
      *
-     * @param deliveryNameRuleId the delivery name rule id
-     * @return the delivery name rule
+     * @param deliveryNameRuleId the delivery name rule id<br>
+     * @return the delivery name rule<br>
      */
     @GetMapping("/{deliveryNameRuleId}")
     public ResponseEntity<DeliveryRuleNameDto> getDeliveryNameRule(
@@ -47,14 +52,24 @@ public class DeliveryRuleNameController {
     }
 
     /**
-     * Create delivery name rule response entity.
+     * methodName : createDeliveryNameRule<br>
+     * author : Fiat_lux<br>
+     * description : post 요청시 {@code request}의 정보를 DB에 저장 시킨 후 DB에 저장된 데이터를 DTO로 반환<br>
+     * {@code request}가 유효성 검사 실패 시 {@code DeliveryRuleNameValidationFailedException}을 던짐<br>
+     * <br>
      *
-     * @param deliveryRuleNameRegisterRequest the delivery name rule register request
-     * @return the response entity
+     * @param deliveryRuleNameRegisterRequest the delivery name rule register request<br>
+     * @param bindingResult                   유효성 검사 실패 시 발생한 오류에 대한 데이터를 가지고 있다<br>
+     * @return the response entity<br>
+     * @throws DeliveryRuleNameValidationFailedException {@code request} 가 유효성 검사 실패시 던짐 <br>
      */
     @PostMapping
     public ResponseEntity<DeliveryRuleNameResponse> createDeliveryNameRule(
-            @RequestBody DeliveryRuleNameRegisterRequest deliveryRuleNameRegisterRequest) {
+            @Valid @RequestBody DeliveryRuleNameRegisterRequest deliveryRuleNameRegisterRequest,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new DeliveryRuleNameValidationFailedException(bindingResult);
+        }
         DeliveryRuleNameResponse deliveryRuleNameResponse =
                 deliveryNameRuleService.registerDeliveryNameRule(deliveryRuleNameRegisterRequest);
 
@@ -62,12 +77,13 @@ public class DeliveryRuleNameController {
     }
 
 
-
     /**
-     * Delete delivery name rule response entity.
+     * methodName : deleteDeliveryNameRule<br>
+     * author : Fiat_lux<br>
+     * description : delete 요청으로 파라미터 값 id에 대한 데이터 삭제<br>
      *
      * @param deliveryNameRuleId the delivery name rule id
-     * @return the response entity
+     * @return void
      */
     @DeleteMapping("/{deliveryNameRuleId}")
     public ResponseEntity<Void> deleteDeliveryNameRule(@PathVariable("deliveryNameRuleId") String deliveryNameRuleId) {
