d20c61f3061728e4ab936b535c9ec4b9a5ac9bf9
minsu <zmfjtnl1@naver.com>
2024-02-27 00:10:10 +0900

    feat: WrapRepository 생성

---
+++ src/main/java/store/mybooks/resource/wrap/repository/WrapRepository.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.wrap.repository;
+
+/**
+ * packageName    : store.mybooks.resource.wrap.repository<br>
+ * fileName       : WrapRepository<br>
+ * author         : minsu11<br>
+ * date           : 2/27/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/27/24        minsu11       최초 생성<br>
+ */
+public interface WrapRepository {
+}
