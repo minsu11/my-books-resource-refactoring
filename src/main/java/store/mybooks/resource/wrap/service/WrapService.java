6ee946280859566b6e2873ce450d307e1e05cb00
minsu <zmfjtnl1@naver.com>
2024-02-27 00:14:37 +0900

    feat: WrapService 생성

---
+++ src/main/java/store/mybooks/resource/wrap/service/WrapService.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.wrap.service;
+
+/**
+ * packageName    : store.mybooks.resource.wrap.service<br>
+ * fileName       : WrapService<br>
+ * author         : minsu11<br>
+ * date           : 2/27/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/27/24        minsu11       최초 생성<br>
+ */
+public class WrapService {
+}
