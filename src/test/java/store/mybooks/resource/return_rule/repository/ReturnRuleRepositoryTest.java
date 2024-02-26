e9dd969a262bf29e47e3ea0985c3d3f9ef5a5a98
minsu <zmfjtnl1@naver.com>
2024-02-26 23:03:02 +0900

    feat: ReturnRuleRepository unit Test 생성

---
+++ src/test/java/store/mybooks/resource/return_rule/repository/ReturnRuleRepositoryTest.java
@@ -0,0 +1,21 @@
+package store.mybooks.resource.return_rule.repository;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * packageName    : store.mybooks.resource.return_rule.repository<br>
+ * fileName       : ReturnRuleRepositoryTest<br>
+ * author         : minsu11<br>
+ * date           : 2/26/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/26/24        minsu11       최초 생성<br>
+ */
+class ReturnRuleRepositoryTest {
+
+    @Test
+    void findByReturnRuleNameId() {
+    }
+}
