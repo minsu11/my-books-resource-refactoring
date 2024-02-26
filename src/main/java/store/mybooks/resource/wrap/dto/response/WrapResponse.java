59df4b705ee0aa6852ee023cba6910a98c21155e
minsu <zmfjtnl1@naver.com>
2024-02-27 00:17:40 +0900

    feat: wrap Response 생성

---
+++ src/main/java/store/mybooks/resource/wrap/dto/response/WrapResponse.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.wrap.dto.response;
+
+/**
+ * packageName    : store.mybooks.resource.wrap.dto.response<br>
+ * fileName       : WrapResponse<br>
+ * author         : minsu11<br>
+ * date           : 2/27/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/27/24        minsu11       최초 생성<br>
+ */
+public class WrapResponse {
+}
