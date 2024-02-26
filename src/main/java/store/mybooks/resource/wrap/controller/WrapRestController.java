bc2ba2b0815b6f2502c22afe2d1ed71189a82afb
minsu <zmfjtnl1@naver.com>
2024-02-27 00:12:01 +0900

    feat: WrapRestController 생성

---
+++ src/main/java/store/mybooks/resource/wrap/controller/WrapRestController.java
@@ -0,0 +1,15 @@
+package store.mybooks.resource.wrap.controller;
+
+/**
+ * packageName    : store.mybooks.resource.wrap.controller<br>
+ * fileName       : WrapRestController<br>
+ * author         : minsu11<br>
+ * date           : 2/27/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/27/24        minsu11       최초 생성<br>
+ */
+public class WrapRestController {
+}
