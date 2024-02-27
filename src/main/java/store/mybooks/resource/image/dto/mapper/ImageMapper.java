5300980a7b8b335775a909c8906c5eb71c64d2cd
hyeonjaez <jhjh0022@naver.com>
2024-02-28 00:26:20 +0900

    feat: ImageMapper

---
+++ src/main/java/store/mybooks/resource/image/dto/mapper/ImageMapper.java
@@ -0,0 +1,21 @@
+package store.mybooks.resource.image.dto.mapper;
+
+import org.mapstruct.Mapper;
+import store.mybooks.resource.image.dto.response.ImageRegisterResponse;
+import store.mybooks.resource.image.entity.Image;
+
+/**
+ * packageName    : store.mybooks.resource.image.dto <br/>
+ * fileName       : ImageMapper<br/>
+ * author         : Fiat_lux <br/>
+ * date           : 2/27/24<br/>
+ * description    :<br/>
+ * ===========================================================<br/>
+ * DATE              AUTHOR             NOTE<br/>
+ * -----------------------------------------------------------<br/>
+ * 2/27/24        Fiat_lux       최초 생성<br/>
+ */
+@Mapper(componentModel = "spring")
+public interface ImageMapper {
+    ImageRegisterResponse mapToResponse(Image image);
+}
