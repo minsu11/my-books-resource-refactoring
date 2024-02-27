3c8ec9d07c70369934f7fbbeb1a9727e2423dfbb
minsu <zmfjtnl1@naver.com>
2024-02-27 22:34:24 +0900

    feat: WrapRestControllerUnitTest 생성

---
+++ src/test/java/store/mybooks/resource/wrap/controller/WrapRestControllerUnitTest.java
@@ -0,0 +1,41 @@
+package store.mybooks.resource.wrap.controller;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * packageName    : store.mybooks.resource.wrap.controller<br>
+ * fileName       : WrapRestControllerUnitTest<br>
+ * author         : minsu11<br>
+ * date           : 2/27/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/27/24        minsu11       최초 생성<br>
+ */
+class WrapRestControllerUnitTest {
+
+    @Test
+    void getWrapResponse() {
+    }
+
+    @Test
+    void getWrapResponsePage() {
+    }
+
+    @Test
+    void getWrapResponseList() {
+    }
+
+    @Test
+    void createWrap() {
+    }
+
+    @Test
+    void modifyWrap() {
+    }
+
+    @Test
+    void deleteWrap() {
+    }
+}
