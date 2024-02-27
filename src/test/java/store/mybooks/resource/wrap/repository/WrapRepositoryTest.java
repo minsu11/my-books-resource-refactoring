2fcbc3931ac3bce182fadac35a5775207debaf23
minsu <zmfjtnl1@naver.com>
2024-02-28 01:34:36 +0900

    feat: WrapRepositoryTest 생성

---
+++ src/test/java/store/mybooks/resource/wrap/repository/WrapRepositoryTest.java
@@ -0,0 +1,16 @@
+package store.mybooks.resource.wrap.repository;
+
+/**
+ * packageName    : store.mybooks.resource.wrap.repository<br>
+ * fileName       : WrapRepositoryTest<br>
+ * author         : minsu11<br>
+ * date           : 2/28/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 2/28/24        minsu11       최초 생성<br>
+ */
+class WrapRepositoryTest {
+
+}
