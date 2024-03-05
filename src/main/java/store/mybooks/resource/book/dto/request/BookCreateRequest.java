3d8094f2ca7f9fd9d8cba4316be5e0c6bfd446bc
newjaehun <sjh030798@gmail.com>
2024-03-05 17:25:26 +0900

    refactorL: BookCreateRequest valid 수정

--- src/main/java/store/mybooks/resource/book/dto/request/BookCreateRequest.java
+++ src/main/java/store/mybooks/resource/book/dto/request/BookCreateRequest.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 import javax.validation.constraints.Size;
@@ -43,7 +43,7 @@ public class BookCreateRequest {
     @Size(min = 13, max = 13)
     private String isbn;
     @NotNull
-    @Past
+    @PastOrPresent
     private LocalDate publishDate;
     @NotNull
     @Positive
