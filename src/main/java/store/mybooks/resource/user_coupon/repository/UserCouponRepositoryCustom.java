d21606a68755d13ab2b8d7182bf337108126aec7
damho <ydh4745@gamil.com>
2024-03-06 19:51:17 +0900

    docs: javadoc 작성

--- src/main/java/store/mybooks/resource/user_coupon/repository/UserCouponRepositoryCustom.java
+++ src/main/java/store/mybooks/resource/user_coupon/repository/UserCouponRepositoryCustom.java
@@ -4,8 +4,8 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.NoRepositoryBean;
-import store.mybooks.resource.user_coupon.dto.response.UserCouponGetResponseForOrderQuerydsl;
 import store.mybooks.resource.user_coupon.dto.response.UserCouponGetResponseForMyPageQuerydsl;
+import store.mybooks.resource.user_coupon.dto.response.UserCouponGetResponseForOrderQuerydsl;
 
 /**
  * packageName    : store.mybooks.resource.user_coupon.repository
@@ -20,9 +20,46 @@ import store.mybooks.resource.user_coupon.dto.response.UserCouponGetResponseForM
  */
 @NoRepositoryBean
 public interface UserCouponRepositoryCustom {
+    /**
+     * methodName : getUserCoupons <br>
+     * author : damho-lee <br>
+     * description : 회원 쿠폰 페이지에 보여줄 쿠폰 페이지 반환.<br>
+     *
+     * @param userId   Long
+     * @param pageable Pageable
+     * @return page
+     */
     Page<UserCouponGetResponseForMyPageQuerydsl> getUserCoupons(Long userId, Pageable pageable);
 
+    /**
+     * methodName : getUsableUserCategoryCouponsByBookId <br>
+     * author : damho-lee <br>
+     * description : 사용 가능한 카테고리 쿠폰 리스트 반환.<br>
+     *
+     * @param userId Long
+     * @param bookId Long
+     * @return list
+     */
     List<UserCouponGetResponseForOrderQuerydsl> getUsableUserCategoryCouponsByBookId(Long userId, Long bookId);
 
+    /**
+     * methodName : getUsableUserBookCouponsByBookId <br>
+     * author : damho-lee <br>
+     * description : 사용 가능한 도서 쿠폰 리스트 반환.<br>
+     *
+     * @param userId Long
+     * @param bookId Long
+     * @return list
+     */
     List<UserCouponGetResponseForOrderQuerydsl> getUsableUserBookCouponsByBookId(Long userId, Long bookId);
+
+    /**
+     * methodName : getUsableTotalCoupons <br>
+     * author : damho-lee <br>
+     * description : 사용 가능한 전체 쿠폰 반환.<br>
+     *
+     * @param userId Long
+     * @return list
+     */
+    List<UserCouponGetResponseForOrderQuerydsl> getUsableTotalCoupons(Long userId);
 }
