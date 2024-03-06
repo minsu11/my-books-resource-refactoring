867efb7038e764a3f49d0b8d1fb040c7b9a36dea
masiljangajji <hjk172262@gmail.com>
2024-03-06 17:48:48 +0900

    refactor: 로그인 로직변경으로 인한 메서드 분리 verifyUserStatus , completeLoginProcess
    verifyUserStatus 유저 존재 , 상태 확인 , 비밀번호 검증은 프론트에서 할 것  , completeLoginProcess 로그인 완료처리

--- src/main/java/store/mybooks/resource/user/controller/UserRestController.java
+++ src/main/java/store/mybooks/resource/user/controller/UserRestController.java
@@ -1,17 +1,11 @@
 package store.mybooks.resource.user.controller;
 
-import java.util.List;
-import javax.ws.rs.POST;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,17 +14,20 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import store.mybooks.resource.config.HeaderProperties;
 import store.mybooks.resource.user.dto.request.UserCreateRequest;
+import store.mybooks.resource.user.dto.request.UserEmailRequest;
 import store.mybooks.resource.user.dto.request.UserGradeModifyRequest;
 import store.mybooks.resource.user.dto.request.UserLoginRequest;
 import store.mybooks.resource.user.dto.request.UserModifyRequest;
+import store.mybooks.resource.user.dto.request.UserOauthLoginRequest;
+import store.mybooks.resource.user.dto.request.UserOauthCreateRequest;
 import store.mybooks.resource.user.dto.request.UserPasswordModifyRequest;
 import store.mybooks.resource.user.dto.request.UserStatusModifyRequest;
 import store.mybooks.resource.user.dto.response.UserCreateResponse;
 import store.mybooks.resource.user.dto.response.UserDeleteResponse;
+import store.mybooks.resource.user.dto.response.UserEncryptedPasswordResponse;
 import store.mybooks.resource.user.dto.response.UserGetResponse;
 import store.mybooks.resource.user.dto.response.UserGradeModifyResponse;
 import store.mybooks.resource.user.dto.response.UserLoginResponse;
@@ -71,11 +68,13 @@ public class UserRestController {
     public ResponseEntity<UserCreateResponse> createUser(
             @RequestBody UserCreateRequest createRequest) {
 
-
         UserCreateResponse createResponse = userService.createUser(createRequest);
 
         return new ResponseEntity<>(createResponse, HttpStatus.CREATED);
     }
+
+
+
 
     /**
      * methodName : modifyUser
@@ -140,9 +139,10 @@ public class UserRestController {
      * @return response entity
      */
     @PutMapping("/password")
-    public ResponseEntity<UserPasswordModifyResponse> modifyUserPassword(@RequestHeader(name = HeaderProperties.USER_ID) Long id,
-                                                                         @RequestBody
-                                                                         UserPasswordModifyRequest modifyRequest) {
+    public ResponseEntity<UserPasswordModifyResponse> modifyUserPassword(
+            @RequestHeader(name = HeaderProperties.USER_ID) Long id,
+            @RequestBody
+            UserPasswordModifyRequest modifyRequest) {
 
         UserPasswordModifyResponse modifyResponse = userService.modifyUserPassword(id, modifyRequest);
         return new ResponseEntity<>(modifyResponse, HttpStatus.OK);
@@ -160,7 +160,6 @@ public class UserRestController {
     @DeleteMapping
     public ResponseEntity<UserDeleteResponse> deleteUser(@RequestHeader(name = HeaderProperties.USER_ID) Long id) {
         UserDeleteResponse deleteResponse = userService.deleteUser(id);
-
         return new ResponseEntity<>(deleteResponse, HttpStatus.OK);
     }
 
@@ -201,15 +200,19 @@ public class UserRestController {
      * author : masiljangajji
      * description : 유저의 로그인을 처리함
      *
-     * @param userLoginRequest login request
      * @return response entity
      */
-    @PostMapping("/login")
-    public ResponseEntity<UserLoginResponse> loginUser(@RequestBody UserLoginRequest userLoginRequest) {
-
-        UserLoginResponse userLoginResponse = userService.loginUser(userLoginRequest);
-        return new ResponseEntity<>(userLoginResponse, HttpStatus.OK);
+    @PostMapping("/verification")
+    public ResponseEntity<UserEncryptedPasswordResponse> verifyUserStatus(@RequestBody UserEmailRequest request) {
+        System.out.println("!!!!");
+        UserEncryptedPasswordResponse userEncryptedPasswordResponse = userService.verifyUserStatusByEmail(request);
+        return new ResponseEntity<>(userEncryptedPasswordResponse, HttpStatus.OK);
     }
 
+    @PostMapping("/verification/complete")
+    public ResponseEntity<UserLoginResponse> completeLoginProcess(@RequestBody UserEmailRequest request) {
+        UserLoginResponse userLoginResponse = userService.completeLoginProcess(request);
+        return new ResponseEntity<>(userLoginResponse, HttpStatus.OK);
+    }
 
 }
