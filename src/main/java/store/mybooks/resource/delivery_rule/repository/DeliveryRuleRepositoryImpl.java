01898a807d21406bcdfb50dca0e4429bc5aa46f0
hyeonjaez <jhjh0022@naver.com>
2024-03-11 20:50:52 +0900

    .

---
+++ src/main/java/store/mybooks/resource/delivery_rule/repository/DeliveryRuleRepositoryImpl.java
@@ -0,0 +1,43 @@
+package store.mybooks.resource.delivery_rule.repository;
+
+import com.querydsl.core.types.Projections;
+import java.util.List;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import store.mybooks.resource.delivery_rule.dto.response.DeliveryRuleResponse;
+import store.mybooks.resource.delivery_rule.entity.DeliveryRule;
+import store.mybooks.resource.delivery_rule.entity.QDeliveryRule;
+
+/**
+ * packageName    : store.mybooks.resource.delivery_rule.repository <br/>
+ * fileName       : DeliveryRuleRepositoryImpl<br/>
+ * author         : Fiat_lux <br/>
+ * date           : 3/11/24<br/>
+ * description    :<br/>
+ * ===========================================================<br/>
+ * DATE              AUTHOR             NOTE<br/>
+ * -----------------------------------------------------------<br/>
+ * 3/11/24        Fiat_lux       최초 생성<br/>
+ */
+public class DeliveryRuleRepositoryImpl extends QuerydslRepositorySupport implements DeliveryRuleRepositoryCustom {
+    public DeliveryRuleRepositoryImpl() {
+        super(DeliveryRule.class);
+    }
+
+    @Override
+    public List<DeliveryRuleResponse> getDeliveryRuleList() {
+        QDeliveryRule deliveryRule = QDeliveryRule.deliveryRule;
+
+        return from(deliveryRule)
+                .select(
+                        Projections.constructor(
+                                DeliveryRuleResponse.class,
+                                deliveryRule.id,
+                                deliveryRule.deliveryRuleName.id,
+                                deliveryRule.companyName,
+                                deliveryRule.cost,
+                                deliveryRule.ruleCost,
+                                deliveryRule.createdDate,
+                                deliveryRule.isAvailable)
+                ).fetch();
+    }
+}
