76b95a8394e4ab64d3f99eb144d38e7f2337cd14
minsu <zmfjtnl1@naver.com>
2024-03-16 22:29:18 +0900

    feat: 주문 관련 필요한 파일 생성

---
+++ src/main/java/store/mybooks/resource/bookorder/service/OrderService.java
@@ -0,0 +1,12 @@
+package store.mybooks.resource.bookorder.service;/**
+ *packageName    : store.mybooks.resource.bookorder.service<br>
+ * fileName       : OrderService<br>
+ * author         : minsu11<br>
+ * date           : 3/16/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 3/16/24        minsu11       최초 생성<br>
+ */public class OrderService {
+}
