b9f92934eb9c51317e7202c9a53d62826fdfadbb
minsu <zmfjtnl1@naver.com>
2024-03-16 14:56:45 +0900

    feat: 주문 상세 repository, service, contorller 생성

---
+++ src/main/java/store/mybooks/resource/order_detail/service/OrderDetailService.java
@@ -0,0 +1,23 @@
+package store.mybooks.resource.order_detail.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import store.mybooks.resource.order_detail.repository.OrderDetailRepository;
+
+/**
+ * packageName    : store.mybooks.resource.order_detail.service<br>
+ * fileName       : OrderDetailService<br>
+ * author         : minsu11<br>
+ * date           : 3/16/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 3/16/24        minsu11       최초 생성<br>
+ */
+@Service
+@RequiredArgsConstructor
+public class OrderDetailService {
+    private final OrderDetailRepository orderDetailRepository;
+}
+
