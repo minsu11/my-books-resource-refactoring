af4666de2e5112d33fbb8e5f878587bcd364f592
minsu <zmfjtnl1@naver.com>
2024-03-16 22:17:33 +0900

    refactor: exception 수정

---
+++ src/main/java/store/mybooks/resource/bookorder/dto/request/BookInfoRequest.java
@@ -0,0 +1,12 @@
+package store.mybooks.resource.bookorder.dto.request;/**
+ *packageName    : store.mybooks.resource.bookorder.dto.request<br>
+ * fileName       : BookInfoRequest<br>
+ * author         : minsu11<br>
+ * date           : 3/16/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 3/16/24        minsu11       최초 생성<br>
+ */public class BookInfoRequest {
+}
