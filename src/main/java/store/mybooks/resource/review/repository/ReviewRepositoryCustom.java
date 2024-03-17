e3110feac2338fe1c2eb438e602d1dfaf379b919
masiljangajji <hjk172262@gmail.com>
2024-03-18 02:16:26 +0900

    feat: ReviewRepositoryCustom , ReviewRepositoryCustomImpl 추가

---
+++ src/main/java/store/mybooks/resource/review/repository/ReviewRepositoryCustom.java
@@ -0,0 +1,29 @@
+package store.mybooks.resource.review.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.NoRepositoryBean;
+import store.mybooks.resource.review.dto.response.ReviewDetailGetResponse;
+import store.mybooks.resource.review.dto.response.ReviewGetResponse;
+
+/**
+ * packageName    : store.mybooks.resource.review.repository<br>
+ * fileName       : ReviewRepositoryCustom<br>
+ * author         : masiljangajji<br>
+ * date           : 3/17/24<br>
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 3/17/24        masiljangajji       최초 생성
+ */
+@NoRepositoryBean
+public interface ReviewRepositoryCustom {
+
+    Page<ReviewGetResponse> getReviewByUserId(Long userId, Pageable pageable);
+
+    Page<ReviewDetailGetResponse> getReviewByBookId(Long bookId, Pageable pageable);
+
+    ReviewGetResponse getReview(Long reviewId);
+
+}
