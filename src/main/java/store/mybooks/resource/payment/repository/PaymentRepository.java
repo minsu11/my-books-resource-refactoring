0d728968f9106ee2dcc3f2186a74132d20339387
minsu <zmfjtnl1@naver.com>
2024-03-19 21:50:14 +0900

    feat: paymentRepository 구현

---
+++ src/main/java/store/mybooks/resource/payment/repository/PaymentRepository.java
@@ -0,0 +1,19 @@
+package store.mybooks.resource.payment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import store.mybooks.resource.payment.entity.Payment;
+
+/**
+ * packageName    : store.mybooks.resource.payment.repository<br>
+ * fileName       : PaymentRepository<br>
+ * author         : minsu11<br>
+ * date           : 3/19/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 3/19/24        minsu11       최초 생성<br>
+ */
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
