175d4ae2f04f234b365ed34a26c057f203c067fd
minsu <zmfjtnl1@naver.com>
2024-03-19 21:49:24 +0900

    feat: pay create dto 구현

---
+++ src/main/java/store/mybooks/resource/payment/dto/request/PayCreateRequest.java
@@ -0,0 +1,25 @@
+package store.mybooks.resource.payment.dto.request;
+
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : store.mybooks.resource.payment.dto.request<br>
+ * fileName       : PayCreateRequest<br>
+ * author         : minsu11<br>
+ * date           : 3/19/24<br>
+ * description    :
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 3/19/24        minsu11       최초 생성<br>
+ */
+@Getter
+@NoArgsConstructor
+public class PayCreateRequest {
+    private String orderNumber;
+    private String status;
+    private LocalDate requestAt;
+    private Integer totalCost;
+}
