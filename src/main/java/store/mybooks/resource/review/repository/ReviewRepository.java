72cd41d386d851d7c2eef3176de158cda316996d
masiljangajji <hjk172262@gmail.com>
2024-03-20 14:18:44 +0900

    feat: ReviewRepository getReviewRate 추가

--- src/main/java/store/mybooks/resource/review/repository/ReviewRepository.java
+++ src/main/java/store/mybooks/resource/review/repository/ReviewRepository.java
@@ -1,6 +1,9 @@
 package store.mybooks.resource.review.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import store.mybooks.resource.review.dto.response.ReviewRateResponse;
 import store.mybooks.resource.review.entity.Review;
 
 /**
@@ -18,5 +21,12 @@ public interface ReviewRepository extends JpaRepository<Review, Long> ,ReviewRep
 
 
     Boolean existsByOrderDetailId(Long orderDetailId);
+
+    @Query("SELECT new store.mybooks.resource.review.dto.response.ReviewRateResponse(COUNT(r), ROUND(AVG(r.rate), 1)) FROM Review r WHERE r.orderDetail.book.id = :bookId")
+    ReviewRateResponse getReviewRate(@Param("bookId") Long bookId);
+
+
+
+
 
 }
