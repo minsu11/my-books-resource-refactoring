b057a7f7f8bb7a5f317170c37fbeb635258b2df8
masiljangajji <hjk172262@gmail.com>
2024-03-20 20:24:40 +0900

    refactor: nativeQuery 삭제

--- src/main/java/store/mybooks/resource/review/repository/ReviewRepository.java
+++ src/main/java/store/mybooks/resource/review/repository/ReviewRepository.java
@@ -1,9 +1,6 @@
 package store.mybooks.resource.review.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import store.mybooks.resource.review.dto.response.ReviewRateResponse;
 import store.mybooks.resource.review.entity.Review;
 
 /**
@@ -17,16 +14,10 @@ import store.mybooks.resource.review.entity.Review;
  * -----------------------------------------------------------<br/>
  * 2/27/24        Fiat_lux       최초 생성<br/>
  */
-public interface ReviewRepository extends JpaRepository<Review, Long> ,ReviewRepositoryCustom{
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
 
 
     Boolean existsByOrderDetailId(Long orderDetailId);
-
-    @Query("SELECT new store.mybooks.resource.review.dto.response.ReviewRateResponse(COUNT(r), ROUND(AVG(r.rate), 1)) FROM Review r WHERE r.orderDetail.book.id = :bookId")
-    ReviewRateResponse getReviewRate(@Param("bookId") Long bookId);
-
-
-
 
 
 }
