9f7e932cdd2a78e2a6a7872529edbee077cdf695
masiljangajji <hjk172262@gmail.com>
2024-03-23 20:15:05 +0900

    feat: PointRule

--- src/main/java/store/mybooks/resource/pointrulename/enumulation/PointRuleNameEnum.java
+++ src/main/java/store/mybooks/resource/pointrulename/enumulation/PointRuleNameEnum.java
@@ -20,6 +20,7 @@ public enum PointRuleNameEnum {
     LOGIN_POINT("로그인 적립"),
     BOOK_POINT("상품 적립"),
     REVIEW_POINT("리뷰 작성 적립"),
+    REVIEW_IMAGE_POINT("리뷰 이미지 작성 적립"),
     SIGNUP_POINT("회원 가입 적립"),
     USE_POINT("포인트 사용");
     private final String value;
