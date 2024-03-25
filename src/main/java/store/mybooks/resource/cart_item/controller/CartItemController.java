4835ba0b8c2a17323cbfc279e7d379318c188be1
hyeonjaez <jhjh0022@naver.com>
2024-03-25 12:47:21 +0900

    fix: mysql에서의 데이터가 삭제가 안되어서 계속 데이터가 있다고 뜸

--- src/main/java/store/mybooks/resource/cart_item/controller/CartItemController.java
+++ src/main/java/store/mybooks/resource/cart_item/controller/CartItemController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -50,5 +52,21 @@ public class CartItemController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(cartDetailList);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> cartItemAllDelete(@RequestHeader(name = HeaderProperties.USER_ID) Long userId) {
+        cartItemService.deleteAllItem(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> cartItemDelete(
+            @RequestHeader(name = HeaderProperties.USER_ID) Long userId,
+            @PathVariable("id") Long id
+    ) {
+        cartItemService.deleteItem(userId, id);
+
+        return ResponseEntity.noContent().build();
     }
 }
